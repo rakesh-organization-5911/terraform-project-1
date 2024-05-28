6a07baaeda2c7bb9033cdf7395b79aff116ffa18
rakeshgithubing <125691946+rakeshgithubing@users.noreply.github.com>
2024-05-28 11:33:05 +0530

    Update main.tf

--- module/main.tf
+++ module/main.tf
@@ -1,6 +1,6 @@
 
 module "ec2" {
-source = "./module/ec2"
+source = "./ec2"
 ami_id = var.ami_id
 instance_type = var.instance_type
 key_pair_name = var.key_pair_name
@@ -10,7 +10,7 @@ iam_role_attach=module.iam.iam_role_attach
 
 
 module "s3" {
-  source= "./module/s3"
+  source= "./s3"
   for_each= var.bucket
   bucketname= each.value.bucketname
 }
